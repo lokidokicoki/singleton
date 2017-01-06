69d4e5765651b7e1cfe2934fc4b4aaba14d266c9
lokidokicoki <lokidokicoki@gmail.com>
2017-01-06 16:02:54 +0000

    new: stitc methods only lib and tests

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import Sing from './sing';
 import Sing2 from './sing2';
+import { Lib as lib } from './lib';
 
 console.log('hello');
+console.log(`constructable singleton`);
 
 const instance2 = new Sing();
 
@@ -13,7 +15,7 @@ const inst = new Sing();
 console.log(inst.time);
 
 console.log(`---`);
-
+console.log(`instanceable singleton`);
 
 const inst3 = Sing2.instance;
 const inst4 = Sing2.instance;
@@ -21,3 +23,15 @@ const inst4 = Sing2.instance;
 console.log(inst3.test1(), inst3.type, inst3.time);
 console.log(inst4.test1(), inst4.type, inst4.time);
 console.log(Sing2.staticTest());
+
+console.log(`---`);
+console.log(`test lib with only static methods`);
+lib.lib1();
+
+console.log(lib.lib2());
+
+console.log(`---`);
+console.log(`create instance of static only lib`);
+console.log('this should fail...');
+let l = new lib();
+l.lib1();
---
+++ src/lib.js
@@ -0,0 +1,9 @@
+export class Lib {
+  static lib1() {
+    console.log(`lib1`);
+  }
+
+  static lib2() {
+    return `lib2`;
+  }
+}
